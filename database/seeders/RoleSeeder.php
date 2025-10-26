bdd33d63b8bf3a5d891c5d1500d05add7d028828
Febrianrifki <febrianrifki1702@gmail.com>
2025-10-26 21:24:14 +0700

    database seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        Role::create([
+            'role_name' => 'Admin',
+            'updated_by' => 1,
+        ]);
+        Role::create([
+            'role_name' => 'User',
+            'updated_by' => 1,
+        ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Admin',
+            'role_id' => 1,
+            'email' => 'admin@admin.com',
+            'password' => 'password',
+            'updated_by' => 1,
+        ]);
+
+        User::create([
+            'name' => 'User',
+            'role_id' => 2,
+            'email' => 'user@user.com',
+            'password' => 'password',
+            'updated_by' => 1,
+        ]);
     }
 }
