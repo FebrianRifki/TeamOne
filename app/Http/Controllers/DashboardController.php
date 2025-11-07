2cf0d3499d3199f50190647f5413680d98eae815
Febrianrifki <febrianrifki1702@gmail.com>
2025-11-07 18:39:22 +0700

    fix query

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,21 +10,22 @@ class DashboardController extends Controller
 {
     public function index()
     {
-       $tasks= Task::selectRaw("status, COUNT(*) as total")
-       ->where("assigned_to", auth()->id())
-       ->groupBy("status")
-       ->pluck("total", "status");
-       $task = $tasks['todo'] ?? 0;
-       $task_in_progress = $tasks['in_progress'] ?? 0;
-       $task_done = $tasks['done'] ?? 0;
-       $total_task = $task + $task_in_progress + $task_done;
+        $tasks = Task::selectRaw("status, COUNT(*) as total")
+            ->where('assigned_to', auth()->user()->id)
+            ->where('deleted_at', null)
+            ->groupBy("status")
+            ->pluck("total", "status");
+        $task = $tasks['todo'] ?? 0;
+        $task_in_progress = $tasks['in_progress'] ?? 0;
+        $task_done = $tasks['done'] ?? 0;
+        $total_task = $task + $task_in_progress + $task_done;
 
-       $todo_task_percent = ($task / $total_task) * 100;
-       $in_progress_task_percent = ($task_in_progress / $total_task) * 100;
-       $done_task_percent = ($task_done / $total_task) * 100;
+        $todo_task_percent = $total_task > 0 ? ($task / $total_task) * 100 : 0;
+        $in_progress_task_percent = $total_task > 0 ? ($task_in_progress / $total_task) * 100 : 0;
+        $done_task_percent = $total_task > 0 ? ($task_done / $total_task) * 100 : 0;
 
-       $project = Project::where("owner_id", auth()->id())->count();
+        $project = Project::where("owner_id", auth()->id())->count();
 
-       return view("dashboard.dashboard", compact("task", "task_in_progress", "task_done", "total_task", "project", "todo_task_percent", "in_progress_task_percent", "done_task_percent"));
+        return view("dashboard.dashboard", compact("task", "task_in_progress", "task_done", "total_task", "project", "todo_task_percent", "in_progress_task_percent", "done_task_percent"));
     }
 }
